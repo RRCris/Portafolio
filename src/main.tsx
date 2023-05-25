4ea66bec1a3df4e4f79d405232b38c1806ce0b8c
RRCris <curiosity.cdrr@gmail.com>
2023-05-24 22:52:10 -0500

    init config to routers

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./Routes/path";
+import ThreePath from "./Routes/ThreePath";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <ThreePath />
+    </BrowserRouter>
   </React.StrictMode>
 );
